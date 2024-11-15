108b4fc525a6eb0735181a9cfa9970ac4443a47f
Tung Hoang Laptop P15 <tufnghd@gmail.com>
2024-11-15 11:49:20 +0700

    error

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,18 +1,30 @@
-import { Outlet } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  console.log(navigation);
+
   return (
-    <div>
-      <Header />
+    <div className="layout">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
 
-      <main>
-        <h1>Content</h1>
-        <Outlet />
-      </main>
+          <main>
+            <h1>Content</h1>
+            <Outlet />
+          </main>
 
-      <CartOverview />
+          <CartOverview />
+        </>
+      )}
     </div>
   );
 }
