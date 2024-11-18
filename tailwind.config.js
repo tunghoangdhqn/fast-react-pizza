874eb811553f906f96202c82771e9dec3f530bde
Tung Hoang Laptop P15 <tufnghd@gmail.com>
2024-11-18 10:59:23 +0700

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace"
+    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
