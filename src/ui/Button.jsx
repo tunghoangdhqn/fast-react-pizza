da0359776e4783bc1f23cafd7f5afeba21a81229
Tung Hoang Laptop P15 <tufnghd@gmail.com>
2024-11-18 16:31:49 +0700

    cart hover

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,8 +7,10 @@ export default function Button({ disabled, children, to, type }) {
     "inline-block rounded-full bg-yellow-500 font-semibold uppercase tracking-wide text-stone-700 outline-none transition-colors duration-300 hover:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-stone-100 sm:py-4 md:px-6";
 
   const styles = {
-    primary: base + "px-4 py-3 md:px-6 md:py-4",
-    small: base + "px-2 py-2 md:px-3 md:py-2 text-xs"
+    primary: base + "px-4 py-4 md:px-6 md:py-4",
+    small: base + "px-2 py-2 md:px-3 md:py-2 text-xs",
+    secondary:
+      "inline-block rounded-full bg-stone-300 font-semibold uppercase tracking-wide text-stone-700 outline-none transition-colors duration-300 hover:bg-stone-400 focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-stone-100 sm:py-4 md:px-6"
   };
 
   if (to)
